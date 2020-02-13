f61111c7cbe39dd2ee339925345d8b68035ee4e2
YorwanOff <61004826+YorwanOff@users.noreply.github.com>
2020-02-13 14:43:15 +0100

    Update VueCreneau.php

--- src/view/VueCreneau.php
+++ src/view/VueCreneau.php
@@ -3,31 +3,41 @@ namespace crazycharlyday\vue;
 
 class VueCreneau extends Vue
 {
+    private $head;
+    private $menu;
+    private $foot;
+
     public function render($sel)
     {
+        switch($sel){
+            case 'LIST' : $menu = $this->listeCreneau();
+        }
         $head = parent::renduTitre();
-        $menu = parent::renduMenu();
         $foot = parent::rendufooter();
 
         echo $head . $menu . $foot;
     }
 
-    public function listCreneau($list){
-        $menu = "";
+    public function listeCreneau($list){
+        $menu = '<div class = creneaux>
+<table>';
         foreach ($list as $key => $value){
             $jour = $value[jour];
             $semaine = $value[semaine];
-            $cycle = $value[cycle];
             $heured = $value[heuredeb];
             $heuref = $value[heurefin];
             $activation = $value[activation];
-            $menu += <<< EOF
-<div class = 'creneaux'>
+            $menu .= <<< EOF
     <tr>
-        <td>{calcDate($semaine, $jour)}</td>
+        <td>Date du créneau : {calcDate($semaine, $jour)}</td>
+        <td>Heure de départ : $heured</td>
+        <td>Heure de fin : $heuref</td>
+        <td>Activation : $activation</td>
     </tr>
 EOF;
 
         }
+        $menu .= '</table>
+</div>';
     }
 }
