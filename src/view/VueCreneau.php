3548486cd9be307aba5dea600f1b1f0eb60b6c71
YorwanOff <61004826+YorwanOff@users.noreply.github.com>
2020-02-13 14:19:12 +0100

    vue creneau

--- src/view/VueCreneau.php
+++ src/view/VueCreneau.php
@@ -11,4 +11,23 @@ class VueCreneau extends Vue
 
         echo $head . $menu . $foot;
     }
+
+    public function listCreneau($list){
+        $menu = "";
+        foreach ($list as $key => $value){
+            $jour = $value[jour];
+            $semaine = $value[semaine];
+            $cycle = $value[cycle];
+            $heured = $value[heuredeb];
+            $heuref = $value[heurefin];
+            $activation = $value[activation];
+            $menu += <<< EOF
+<div class = 'creneaux'>
+    <tr>
+        <td>{calcDate($semaine, $jour)}</td>
+    </tr>
+EOF;
+
+        }
+    }
 }
